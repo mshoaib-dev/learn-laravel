5cd079cf1448d1b7dbe71510beb99204b4726e56
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-07 00:12:01 -0700

    job-model creation & handling-logic

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,8 @@
 <?php
+namespace App\Models;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-// defining separate method in a class for jobs-listings
-class Job {
-    public static function all(): array // explicitly defining return type
-    {
-        return  [
-            [
-                "id" => 1,
-                "title"=>"Director General",
-                "Salary"=>"$50K",
-                "Experience"=> 10
-            ],
-            [
-                "id" => 2,
-                "title"=>"Programmer",
-                "Salary"=>"$45K",
-                "Experience"=> 3
-            ],
-            [
-                "id" => 3,
-                "title"=>"HR",
-                "Salary"=>"$40K",
-                "Experience"=> 5
-            ]
-        ];
-    }
-}
-
-
 
 Route::get('/', function () {
     return view('Home');
@@ -41,11 +13,10 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ["jobs"=>Job::all()]);
+    return view('jobs', ["jobs" => Job::all()]);
 });
 
-// you may explicitly define function return type of ARRAY or format the return data in a array format as given
 Route::get('/jobs/{id}', function ($id) {
-    $job = Arr::first(Job::all(), fn($job)=>$job['id'] == $id);
+   $job = Job::find($id);
    return view('job', ["job"=>$job]);
 });
