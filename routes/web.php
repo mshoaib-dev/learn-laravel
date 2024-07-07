4d6255a5db96e9f8612fc980bb82c06e1ab86cf0
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-06 22:18:37 -0700

    class-Method-based-Logic

--- routes/web.php
+++ routes/web.php
@@ -5,32 +5,32 @@ use Illuminate\Support\Facades\Route;
 
 // defining separate method in a class for jobs-listings
 class Job {
-    public static function all()
+    public static function all(): array // explicitly defining return type
     {
-
+        return  [
+            [
+                "id" => 1,
+                "title"=>"Director General",
+                "Salary"=>"$50K",
+                "Experience"=> 10
+            ],
+            [
+                "id" => 2,
+                "title"=>"Programmer",
+                "Salary"=>"$45K",
+                "Experience"=> 3
+            ],
+            [
+                "id" => 3,
+                "title"=>"HR",
+                "Salary"=>"$40K",
+                "Experience"=> 5
+            ]
+        ];
     }
 }
 
-$jobs = [
-    [
-        "id" => 1,
-        "title"=>"Director General",
-        "Salary"=>"$50K",
-        "Experience"=> 10
-    ],
-    [
-        "id" => 2,
-        "title"=>"Programmer",
-        "Salary"=>"$45K",
-        "Experience"=> 3
-    ],
-    [
-        "id" => 3,
-        "title"=>"HR",
-        "Salary"=>"$40K",
-        "Experience"=> 5
-    ]
-];
+
 
 Route::get('/', function () {
     return view('Home');
@@ -40,12 +40,12 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs', function () use ($jobs) {
-    return view('jobs', ["jobs"=>$jobs]);
+Route::get('/jobs', function () {
+    return view('jobs', ["jobs"=>Job::all()]);
 });
 
 // you may explicitly define function return type of ARRAY or format the return data in a array format as given
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = Arr::first($jobs, fn($job)=>$job['id'] == $id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Arr::first(Job::all(), fn($job)=>$job['id'] == $id);
    return view('job', ["job"=>$job]);
 });
