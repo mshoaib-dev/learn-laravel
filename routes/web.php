fd93f83bdc991972cdcc5dbfb859073ff82c8ff5
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-06 22:14:26 -0700

    separating the jobs array at single place

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,35 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+// defining separate method in a class for jobs-listings
+class Job {
+    public static function all()
+    {
+
+    }
+}
+
+$jobs = [
+    [
+        "id" => 1,
+        "title"=>"Director General",
+        "Salary"=>"$50K",
+        "Experience"=> 10
+    ],
+    [
+        "id" => 2,
+        "title"=>"Programmer",
+        "Salary"=>"$45K",
+        "Experience"=> 3
+    ],
+    [
+        "id" => 3,
+        "title"=>"HR",
+        "Salary"=>"$40K",
+        "Experience"=> 5
+    ]
+];
+
 Route::get('/', function () {
     return view('Home');
 });
@@ -11,60 +40,12 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs', function () {
-    return view('jobs',
-        [
-            "jobs" => [
-                [
-                    "id" => 1,
-                    "title"=>"Director General",
-                    "Salary"=>"$50K",
-                    "Experience"=> 10
-                ],
-                [
-                    "id" => 2,
-                    "title"=>"Programmer",
-                    "Salary"=>"$45K",
-                    "Experience"=> 3
-                ],
-                [
-                    "id" => 3,
-                    "title"=>"HR",
-                    "Salary"=>"$40K",
-                    "Experience"=> 5
-                ]
-            ]
-        ]);
+Route::get('/jobs', function () use ($jobs) {
+    return view('jobs', ["jobs"=>$jobs]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            "id" => 1,
-            "title"=>"Director General",
-            "Salary"=>"$50K",
-            "Experience"=> 10
-        ],
-        [
-            "id" => 2,
-            "title"=>"Programmer",
-            "Salary"=>"$45K",
-            "Experience"=> 3
-        ],
-        [
-            "id" => 3,
-            "title"=>"HR",
-            "Salary"=>"$40K",
-            "Experience"=> 5
-        ]
-    ];
-
-//    Arr::first($jobs, function ($job) use ($id) {
-//        return $job['id'] == $id;
-//    });
-
-    // OR, using arrow function
-
+// you may explicitly define function return type of ARRAY or format the return data in a array format as given
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $job = Arr::first($jobs, fn($job)=>$job['id'] == $id);
    return view('job', ["job"=>$job]);
 });
