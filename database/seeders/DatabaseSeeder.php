e15167ffb9dd11d058d261b0eedf1d53a27ae9c1
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-16 07:14:12 -0700

    all-about seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // User::factory(10)->create(); // for multiple data set.
+        // also order of functions given matters too.
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'first_name' => 'M. ',
+            'last_name' => 'Shoaib',
+            'email' => 'mshoaib@dev.com',
         ]);
+
+//        Job::factory(5)->create();
+        // OR call the relevant seeder class here.
+        // OR trigger the relevant seeder class here, i.e., php artisan db:seed --class = <name of Seeder class>
+        $this->call(JobSeeder::class);
     }
 }
