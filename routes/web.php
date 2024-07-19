ebf68ec3471757220c7c1b7d00f708339c215f19
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-18 23:54:33 -0700

    RouteGroup fun-implemented

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,17 @@ namespace App\Models;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
 Route::view('/', 'home');
+// Routes-Group
+Route::controller(JobController::class)->group(function (){
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}', 'show');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
+
 Route::view('/contact', 'contact');
 
