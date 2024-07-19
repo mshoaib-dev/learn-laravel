857db3889d57f56076bfeeacf1a8d058b5aa3dab
Muhammad Shoaib <mmshooaib@gmail.com>
2024-07-18 23:07:10 -0700

    ControllerClasses initiated

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employer;
+use App\Models\Job;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->simplePaginate(4);
+        return view('jobs.index', ["jobs" => $jobs]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function store()
+    {
+        //validate
+        request()->validate([
+            'title'=>['required', 'min:3'],
+            'salary'=>['required'],
+            'experience'=>['required', 'integer'],
+        ]);
+//store in DB
+        Job::create([
+            "employer_id"=>Employer::find(5)->id,
+            "title"=>request('title'),
+            "salary"=>request('salary'),
+            "experience"=>request('experience'),
+        ]);
+//redirect
+        return redirect("/jobs");
+
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ["job"=>$job]);
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', ["job"=>$job]);
+    }
+
+    public function update(Job $job)
+    {
+        //authorize ... later
+        //validate
+        request()->validate([
+            'title'=>['required', 'min:3'],
+            'salary'=>['required'],
+            'experience'=>['required', 'integer'],
+        ]);
+        //update
+        $job->update([
+            'title'=>request('title'),
+            'salary'=>request('salary'),
+            'experience'=>request('experience'),
+        ]);
+        //persist update() will automatically save in the DB
+        //redirect
+        return redirect('/jobs/'.$job['id']);
+    }
+
+    public function destroy(Job $job)
+    {
+        $job->delete();
+        return redirect('/jobs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 namespace App\Models;
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 Route::get('/', function () {
     return view('home');
@@ -11,67 +20,3 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-//index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(4);
-    return view('jobs.index', ["jobs" => $jobs]);
-});
-
-//create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-//store
-Route::post('/jobs', function () {
-//validate
-    request()->validate([
-        'title'=>['required', 'min:3'],
-        'salary'=>['required'],
-        'experience'=>['required', 'integer'],
-    ]);
-//store in DB
-    Job::create([
-      "employer_id"=>Employer::find(5)->id,
-      "title"=>request('title'),
-      "salary"=>request('salary'),
-      "experience"=>request('experience'),
-    ]);
-//redirect
-    return redirect("/jobs");
-});
-
-//show
-Route::get('/jobs/{job}', function (Job $job) {
-   return view('jobs.show', ["job"=>$job]);
-});
-
-//edit
-Route::get('/jobs/{job}/edit', function (Job $job) {
-    return view('jobs.edit', ["job"=>$job]);
-});
-
-//update
-Route::patch('/jobs/{job}', function (Job $job) {
-    //authorize ... later
-    //validate
-    request()->validate([
-        'title'=>['required', 'min:3'],
-        'salary'=>['required'],
-        'experience'=>['required', 'integer'],
-    ]);
-    //update
-    $job->update([
-        'title'=>request('title'),
-        'salary'=>request('salary'),
-        'experience'=>request('experience'),
-    ]);
-    //persist update() will automatically save in the DB
-    //redirect
-    return redirect('/jobs/'.$job['id']);
-});
-//destroy
-Route::delete('/jobs/{job}', function (Job $job) {
-    $job->delete();
-    return redirect('/jobs');
-});
