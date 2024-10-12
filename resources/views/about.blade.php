565fa80127303d55d07a679c4a212e9350039b9c
Muhammad Shoaib <mmshooaib@gmail.com>
2024-10-12 22:34:25 +0500

    nav-link updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
-    <a href = '/'>Home</a>
-    <a href = '/about'>About</a>
-    <a href = '/contact'>Contact</a>
+    <x-nav></x-nav>
     <h1>Welcome on About-us Page.</h1>
 </x-layout>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <nav>
-        {{ $slot }}
-    </nav>
-</body>
-</html>
-
-
-
-
+<a href = '/'>Home</a>
+<a href = '/about'>About</a>
+<a href = '/contact'>Contact</a>
